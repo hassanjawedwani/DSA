08e1e6a28620dfd7ba72f9ab468adb08caf1b97e
firstRepo <hassanjawedwani@gmail.com>
2024-07-08 00:08:53 +0100

    checkpoint

---
+++ script.js
@@ -0,0 +1,80 @@
+// problem 1
+
+// const choice = prompt("Enter number: ");
+// for (let i = 1; i <= choice; i++) {
+//   let str = "";
+//   for (let j = 1; j <= choice; j++) {
+//     str += choice-j+1;
+//   }
+//   console.log(str);
+// }
+
+// for (let row = 4; row >= 1; row--) {
+//   let r = "";
+//   for (let col = 4; col >= 1; col--) {
+//     r += col;
+//   }
+//   console.log(r)
+// }
+
+// const choice = prompt("Enter number: ");
+// for (let i = 1; i <= choice; i++) {
+//   let str = "";
+//   for (let j = 1; j <= choice; j++) {
+//     str += j;
+//   }
+//   console.log(str);
+// }
+
+// let cnt = 0;
+// for (let i = 1; i <= 3; i++) {
+//     let str = "";
+//   for (let j = 1; j <= 3; j++) {
+//       cnt++;
+//       str += cnt;
+//   }
+//   console.log(str);
+// }
+
+
+// for (let i = 0; i < 3; i++) {
+//   let str = "";
+//   for (let j = 0; j < 3; j++) {
+//     str += (j + 1) +(3 * i)
+//   }
+//   console.log(str);
+// }
+
+// for (let i = 1; i <= 3; i++) {
+//   console.log("*".repeat(i));
+// }
+
+// const choice = prompt("Enter number: ");
+// for (let i = 1; i <= choice; i++) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str += "*";
+//   }
+//   console.log(str);
+// }
+
+
+// const choice = prompt("Enter number: ");
+// for (let i = 1; i <= choice; i++) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str += i;
+//   }
+//   console.log(str);
+// }
+
+
+
+
+// for (let i = 1; i <= 4; i++) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str += i + j - 1;
+//   }
+//   console.log(str);
+// }
